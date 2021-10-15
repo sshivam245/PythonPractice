daa062571df4a80b64a55ec37aa99722484fd563
shivam goel <shivamgoel@shivams-MacBook-Air.local>
2021-10-16 00:45:26 +0530

    area

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+A = 5
+B = 7
+C = 10
+s = (A + B + C) / 2
+Area = (s*(s-A)*(s-B)*(s-C)) ** 0.5
+print(f"area of triangle is :   {Area}")
---
+++ circle area.py
@@ -0,0 +1,6 @@
+r = float(input("enter the value of r!!!"))
+pi = 3.14
+area_circle = pi*r*r
+print(f"area of circle is:   {area_circle} " )
+
+

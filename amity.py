7fb7affeec85af4912c1c0d0fdf05878e3268912
shivam goel <shivamgoel@shivams-MacBook-Air.local>
2021-10-15 14:32:46 +0530

    Intial commit

---
+++ amity.py
@@ -0,0 +1,25 @@
+def BMI(height, weight):
+    bmi = weight/(height**2)
+    return bmi
+ 
+
+height = 1.79832
+weight = 85
+ 
+
+bmi = BMI(height, weight)
+print("The BMI is", format(bmi), "so ", end='')
+ 
+
+if (bmi < 18.5):
+    print("underweight")
+ 
+elif ( bmi >= 18.5 and bmi < 24.9):
+    print("Healthy")
+ 
+elif ( bmi >= 24.9 and bmi < 30):
+    print("overweight")
+ 
+elif ( bmi >=30):
+    print("Suffering from Obesity")
+    

5c3153b0b0e42cccb9ccd00e57b5e31c0327fe6c
shivam goel <shivamgoel@shivams-MacBook-Air.local>
2021-10-20 22:56:01 +0530

    update area of triangle

--- area of triangle.py
+++ area of triangle.py
@@ -1,6 +1,6 @@
-A = 5
-B = 7
-C = 10
+A = int(input("what is the value of A:  "))
+B = int(input("what is the value of B:  "))
+C = int(input("what is the value of C:  "))
 s = (A + B + C) / 2
 Area = (s*(s-A)*(s-B)*(s-C)) ** 0.5
 print(f"area of triangle is :   {Area}")
